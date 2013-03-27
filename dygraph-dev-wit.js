2b877d515f3c4cace28ad95b2ca8efc41102b80d
Emmanuel Fitoussi <manu.fitoussi@gmail.com>
2013-03-27 16:14:57 +0100

    clean loader script.

--- dygraph-dev-wit.js
+++ dygraph-dev-wit.js
@@ -26,17 +26,16 @@
     "dygraph-canvas.js",
     "dygraph.js",
     "dygraph-utils.js",
-    //"dygraph-gviz.js",
     "dygraph-interaction-model.js",
     "dygraph-tickers.js",
-    "plugins/base.js",
+    "dygraph-plugin-base.js",
     "plugins/annotations.js",
     "plugins/axes.js",
     "plugins/chart-labels.js",
     "plugins/grid.js",
     "plugins/legend.js",
     "plugins/range-selector.js",
-    "plugins/install.js",
+    "dygraph-plugin-install.js",
     "dygraph-options-reference.js"  // Shouldn't be included in generate-combined.sh
   ];
 
